9cd66cdfa7411cb6cc8847ed32578b9284f9523d
EpicJoseph <73321523+EpicJoseph@users.noreply.github.com>
2020-10-28 17:34:52 -0700

    Update MazeGenerator.java

--- MazeGenerator/src/mazegenerator/MazeGenerator.java
+++ MazeGenerator/src/mazegenerator/MazeGenerator.java
@@ -62,8 +62,8 @@ public class MazeGenerator {
         printPath();
         
         
-        // R Chen - My new maze. Dimensions are 15 x 15.
-        n = 15;
+        // Joseph Chang - My new maze. Dimensions are 40 x 40.
+        n = 40;
         makeMaze();
         printMaze();
         printPath();
